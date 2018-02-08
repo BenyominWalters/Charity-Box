12a88df81827c214add9af10778546da80c83d45
Benjamin <riversplitter@gmail.com>
2018-02-08 14:56:27 -0600

    Wired up my pages instead of defaul pages.

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { BoxPage } from '../box/box';
+import { SettingsPage } from '../settings/settings';
+import { HistoryPage } from '../history/history';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = HistoryPage;
+  tab2Root = BoxPage;
+  tab3Root = SettingsPage;
 
   constructor() {
 
