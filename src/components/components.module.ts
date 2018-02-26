deef90edab6e2f1e522cb33beddca2dc9167bfe7
Benjamin <riversplitter@gmail.com>
2018-02-26 14:35:39 -0600

    Styled buttons. Added image of box.

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { FaIconComponent } from './fa-icon/fa-icon';
+import { IonicModule } from "ionic-angular";
 @NgModule({
 	declarations: [FaIconComponent],
-	imports: [],
+	imports: [IonicModule],
 	exports: [FaIconComponent]
 })
 export class ComponentsModule {}
